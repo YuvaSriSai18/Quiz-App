ea12d43dda1bdeea3e700a4eecb68addc316ffbd
Yuva Sri Sai <nanithota18102004@gmail.com>
2024-08-10 16:03:39 +0530

    last and final commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,32 +2,8 @@ import React, { useEffect } from "react";
 import ResponsiveAppBar from "./components/Navbar/ResponsiveAppBar";
 import AllRoutes from "./AllRoutes";
 import { BrowserRouter } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { setUserData } from "./reducers/authentication/authSlice";
-import { setLeaderBoardUserData } from "./reducers/LeaderBoard/LeaderBoardSlice";
-import axios from "axios";
 
 function App() {
-  const dispatch = useDispatch();
-  const userData = useSelector((state) => state.auth.userData);
-
-  // const getUser = async () => {
-  //   try {
-  //     const response = await axios.get(`http://localhost:5500/login/success`, {
-  //       withCredentials: true,
-  //     });
-  //     // console.log("res", response);
-  //     dispatch(setUserData(response.data.user));
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   getUser();
-  // }, []);
-
-
   return (
     <BrowserRouter>
       <ResponsiveAppBar />
